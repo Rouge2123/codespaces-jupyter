439fa047a63fc2d3ca23d3c8db07d46f40afb142
Rouge2123 <112276238+Rouge2123@users.noreply.github.com>
2023-12-16 23:46:01 +0100

    Getting used to the syntax

---
+++ smallSteps/codecademy1.py
@@ -0,0 +1,15 @@
+# Following the Codecademy Python 3 course [https://www.codecademy.com/enrolled/courses/learn-python-3]
+
+# Starting basic :)
+print("Hello World")
+
+# Declaring and changing variables
+meal = "Breakfast"
+print(meal)
+
+meal = "Lunch"
+print(meal)
+
+meal = "Dinner"
+print(meal)
+
