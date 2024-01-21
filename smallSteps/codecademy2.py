6049577d6d739a0bdc3654ea106d1149dfa742ff
Rouge2123 <112276238+Rouge2123@users.noreply.github.com>
2024-01-21 10:35:15 +0100

    Practicing if-statements with user input

---
+++ smallSteps/codecademy2.py
@@ -0,0 +1,24 @@
+# Interplanetary boxer keeping track of his weight on different planets
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+weight = 98
+
+planet = int(input("Which planet are you on? (Pick a number from the list above)"))
+
+if planet == 1:
+  weight = weight * 0.91
+elif planet == 2:
+  weight = weight * 0.38
+elif planet == 3:
+  weight = weight * 2.34
+elif planet == 4:
+  weight = weight * 1.06
+elif planet == 5:
+  weight = weight * 0.92
+elif planet == 6:
+  weight = weight * 1.19
+
+print("Your weight is", weight)
